c66bb84aac025647c68a6db9fe06e10e7f06749c
João Carlos Casoto Júnior <58753429+jrcasoto@users.noreply.github.com>
2021-10-27 15:47:19 -0300

    Update repo

---
+++ C/filter/helpers.c
@@ -0,0 +1,122 @@
+#include "helpers.h"
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
+            image[i][j].rgbtBlue = (int)round(avg);
+            image[i][j].rgbtGreen = (int)round(avg);
+            image[i][j].rgbtRed = (int)round(avg);
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int cap = 255;
+            float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            image[i][j].rgbtBlue = round(sepiaBlue);
+            image[i][j].rgbtGreen = round(sepiaGreen);
+            image[i][j].rgbtRed = round(sepiaRed);
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width / 2); j++)
+        {
+            RGBTRIPLE tmp = image[i][j];
+            image[i][j] = image[i][width-j-1];
+            image[i][width-j-1] = tmp;
+        }
+    }
+    return;
+}
+
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Temporary storage
+    RGBTRIPLE temp[height][width];
+
+    // Copying the image to keep an unaltered version to loop over
+    memcpy(temp, image, sizeof(RGBTRIPLE) * height * width);
+
+    // Iterate over every row of the image
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over every column of the image
+        for (int j = 0; j < width; j++)
+        {
+            // Initiate average counter at 0.0
+            // Gotta avoid the truncated integer problem
+            float average = 0.0;
+            // Initiate RGB values at 0
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            // Iterate over rows around current row
+            for (int k = -1; k <= 1; k++)
+            {
+                // Iterate over columns around current column
+                for (int l = -1; l <= 1; l++)
+                {
+                    // If current row + next row are within bounds
+                    // If current column + next column are within bounds
+                    if (i + k != height &&
+                        i + k != -1 &&
+                        j + l != width &&
+                        j + l != -1)
+                    {
+                        // Update RGB values to the sum both pixels' RGB values
+                        red += temp[i + k][j + l].rgbtRed;
+                        green += temp[i + k][j + l].rgbtGreen;
+                        blue += temp[i + k][j + l].rgbtBlue;
+                        // Increment average by one for one pixel in the sum
+                        average++;
+                    }
+                }
+            }
+            // Set each RGB values to their blurred values
+            image[i][j].rgbtRed = round(red / average);
+            image[i][j].rgbtGreen = round(green / average);
+            image[i][j].rgbtBlue = round(blue / average);
+        }
+    }
+}
